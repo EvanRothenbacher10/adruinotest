da12c15ab3a589a745e1950898942abc7b1a8aac
EvanRothenbacher10 <825585@mystma.org>
2022-10-10 13:32:06 -0500

    1

--- code.py
+++ code.py
@@ -1,11 +1,30 @@
 from adafruit_circuitplayground import cp
 import time
+import copy
 import threading
+r = 0
+g = 127
+b = 254
 
-def changeColor:
-    cp.pixels[0]
+def changeColor():
+    global r, g, b
+    while True:
+        r = r + 1
+        time.sleep(0.5)
+        g = g + 1
+        time.sleep(0.5)
+        b = b + 1
+        if r == 255:
+            r = 0
+        elif g == 255:
+            g = 0
+        elif b == 255:
+            b = 255
+        time.sleep(0.5)
 
-def setColor:
-    pass
+def setColor():
+    cp.pixels[0] = (r, g, b)
+    cp.pixels[1] = (copy.copy(r), copy.copy(g), copy.copy(b))
+    cp.pixels[2] = (copy.copy)
 
-changeThread = threading.thread(target= changeColor())
+changeThread = threading.thread(target= changeColor)
