ed8308ed2f8a6e7fae36cb5448e676c80480b71b
Evan <81447921+EvanRothenbacher10@users.noreply.github.com>
2022-10-10 09:49:55 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+from adafruit_circuitplayground import cp
+import time
+import threading
+
+def changeColor:
+    cp.pixels[0]
+
+def setColor:
+    pass
+
+changeThread = threading.thread(target= changeColor())
